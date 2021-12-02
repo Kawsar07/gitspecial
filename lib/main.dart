9533285c3d3435ec330031065be8a9aeec578719
kawsar07 <kawsar.ali.cse@gmail.com>
2021-12-02 19:44:50 +0600

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        // new
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
