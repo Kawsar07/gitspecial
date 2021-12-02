6289cedb839d1ee50607845162d076b2427f2b94
kawsar07 <kawsar.ali.cse@gmail.com>
2021-12-02 19:46:02 +0600

    worhg

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        canvasColor: C,
         // new
         // This is the theme of your application.
         //
